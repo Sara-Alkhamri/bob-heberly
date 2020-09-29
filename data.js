2b9206bc6060e04276bd5df23948edb0f23330de
Sara Alkhamri <s.alkhamri@gmail.com>
2020-09-29 13:19:01 -0700

    Created storeProduct array in data.js and added corresponding images in img folder

---
+++ data.js
@@ -0,0 +1,152 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Annie",
+    img: "img/product-1.jpg",
+    price: "1200",
+    info: "Annie on a bed",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Chairs",
+    img: "img/product-2.jpg",
+    price: "1400",
+    info: "Backyard Chairs",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "June 16 2016",
+    img: "img/product-3.jpg",
+    price: "2100",
+    info: "June 16 2016",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Color",
+    img: "img/product-4",
+    price: "600",
+    info: "Color",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Chair",
+    img: "img/product-5",
+    price: "500",
+    info: "Vintage Chair",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Red Banana",
+    img: "img/product-6",
+    price: "1100",
+    info: "Red Banana",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "French Door",
+    img: "img/product-7",
+    price: "2200",
+    info: "Home",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Bobcat",
+    img: "img/product-8",
+    price: "1200",
+    info: "Bobcat on yellow",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 9,
+    title: "Norcineria",
+    img: "img/product-9",
+    price: "3000",
+    info: "A day at Norcineria",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 10,
+    title: "French Doors",
+    img: "img/product-10",
+    price: "2200",
+    info: "French Doors",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 11,
+    title: "Backyard",
+    img: "img/product-11",
+    price: "1300",
+    info: "Sundays in the backyard",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 12,
+    title: "Patio Bedroom",
+    img: "img/product-12",
+    price: "1500",
+    info: "Patio Bedroom",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 13,
+    title: "Pool",
+    img: "img/product-13",
+    price: "1100",
+    info: "Pool",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 14,
+    title: "Red Chair",
+    img: "img/product-14",
+    price: "900",
+    info: "Red Chair",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 15,
+    title: "Yard Chair",
+    img: "img/product-15",
+    price: "1000",
+    info: "Yard Chair",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
