5b87ca03ed85e09143f4fbc3418f22a06abe7dfa
Sara Alkhamri <s.alkhamri@gmail.com>
2020-09-29 14:40:01 -0700

    Added Link to Home.js for ProductList

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
     <React.Fragment>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/art" component={ProductList} />
       </Switch>
     </React.Fragment>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="row">
       <div className="col-10 mx-auto my-2 text-center text-title">
-        <h1>Hello from Home</h1>
+        <h1>ROBERT HEBERLY</h1>
+        <h3>PASTEL AND OIL</h3>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/art" className="nav-link">
+              Enter
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
