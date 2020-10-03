53f8c15513d2aff9e86ca6d86e9fbafb6060f3a1
Sara Alkhamri <s.alkhamri@gmail.com>
2020-10-02 19:05:08 -0700

    Created Cart component and imported CartColumns and EmptyCart components to it

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
-
+import CartColumns from "./CartColumns";
 import { ProductConsumer } from "../../context";
+import EmptyCart from "./EmptyCart";
 
 export default class Cart extends Component {
   render() {
-    return <h1>Hello from cart</h1>;
+    return (
+      <section>
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <CartColumns />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
+      </section>
+    );
   }
 }
