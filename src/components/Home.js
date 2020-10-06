ce4e33adb554d5699b6ce169a36e69b680c3ddd8
Sara Alkhamri <s.alkhamri@gmail.com>
2020-10-06 16:03:43 -0700

    Updated styling for Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,49 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 import bob from "../bob-painting.png";
 import bobLogo from "../bob.png";
-
+import { ButtonContainer } from "./Button";
 import styled from "styled-components";
 
 export default function Home() {
   return (
-    <HomeNav className="container">
-      <img src={bobLogo} alt="store" className="" />
-
-      <div className="content">
-        <div className="text-content">
+    <HomeNav classname="py-5">
+      <div className="navbar navbar-expand-sm px-sm-5">
+        <img src={bobLogo} alt="store" />
+      </div>
+      <div className="content d-flex justify-content-around align-items-center">
+        <div className="">
           <h3 className="">Pastel and Oil</h3>
           <p>is simply dummy text of the printing and typesetting industry.</p>
-
-          <Link to="/art" className="nav-link">
-            Art Collection
-          </Link>
+          <ButtonContainer>
+            <Link to="/art" className="nav-link">
+              Art Collection
+            </Link>
+          </ButtonContainer>
         </div>
-        <img src={bob} alt="bob painting" className="bob-img" />
+        <img src={bob} alt="bob painting" />
       </div>
     </HomeNav>
   );
 }
 
 const HomeNav = styled.nav`
-  .container {
-    background: var(--white);
-
-    display: flex;
-  }
-  .nav-link {
-    color: var(--etonBlue) !important;
-    font-size: 2rem;
-    text-transform: capitalize;
-    text-decoration-line: underline;
-    /* margin-left: 3rem; */
-  }
   .content {
+    color: var(--etonBlue);
   }
-  .text-content {
-    display: inline-flex;
-    flex-direction: column;
-  }
-  .bob-img {
-    transform: translate(22em, 0);
+
+  .nav-link {
+    color: var(--beige) !important;
+    font-size: 1.5rem;
+    text-transform: capitalize;
+    margin-left: 0;
   }
 `;
