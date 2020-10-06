da948fec3e711a6f4e2b51c338c68542bc39dcdf
Sara Alkhamri <s.alkhamri@gmail.com>
2020-10-05 19:55:19 -0700

    Added links to Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
-  return <div>This is Footer</div>;
+  return (
+    <div className="navbar-nav align-items-left">
+      <Link to="/">Home</Link>
+      <Link to="/art">Art</Link>
+      <Link to="/cart">Cart</Link>
+      <div className="text">
+        {/* <small>Created by Sara Alkhamri 2020</small> */}
+      </div>
+    </div>
+  );
 }
